0420733ec4d4e2b8579d9d9a95e5de3528879603
NestTiga <nestortigasi371@gmail.com>
2023-12-22 17:56:14 -0500

    Inicio terminado, creación de imc

--- app/src/main/java/com/tigasinestor/appnest/MainActivity.kt
+++ app/src/main/java/com/tigasinestor/appnest/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.tigasinestor.appnest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.tigasinestor.imcDesarrollo.ImcNestorTigasiActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Capturamos al botón iniciar y lanzamos
+        val btnIniciar= findViewById<Button>(R.id.btnIniciar)
+
+        //Evento
+        btnIniciar.setOnClickListener {
+            Log.i("BOTÓN --------- ","LANZAMOS APP IMC")
+            navegarHaciaApp(ImcNestorTigasiActivity::class.java)
+        }
+
+    }
+
+    fun navegarHaciaApp(clase:Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
